4fb61f437aba6952852a5cbc60c1cdf6438f4594
asalinas9 <salinas99@live.com>
2020-06-11 18:12:16 -0500

    set controllers/models

--- server.js
+++ server.js
@@ -1,27 +1,25 @@
-require('dotenv').config();
-const express = require("express");
-const app = express();
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
 }
 
-const mongoose = require("mongoose");
-const mongoURL = process.env.PROD_MONGODB || "mongodb://localhost:27017/googlebooks"
-mongoose.connect(mongoURL, {useNewUrlParser: true})
-  .then(() => {
-    console.log("🗄 ==> Successfully connected to mongoDB.");
-  })
-  .catch((err) => {
-    console.log(`Error connecting to mongoDB: ${err}`);
-  });
+// Define API routes here
+app.use(routes);
 
-require("./routes/api-routes")(app);
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {useNewUrlParser: true});
 
 app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+	console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
